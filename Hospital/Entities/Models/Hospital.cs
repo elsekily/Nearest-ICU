3691a50a252ce1cc01a3bd667c3995631d6731e9
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-02 22:14:59 +0200

    Adding Hospital Enitiy

---
+++ Hospital/Entities/Models/Hospital.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Entities.Models
+{
+    public class Hospital
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Address { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+        public int FreeUnits { get; set; }
+    }
+}
