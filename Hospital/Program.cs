c3a0229b4e54141a90c2c8990011d7dab5da0a2e
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-03 18:18:32 +0200

    updating seed

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -2,6 +2,7 @@ using Hospital.Data;
 using Hospital.Entities.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,8 +16,10 @@ namespace Hospital
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<HospitalsDbContext>();
                 var userManager = services.GetRequiredService<UserManager<User>>();
                 var roleManager = services.GetRequiredService<RoleManager<Role>>();
+                context.Database.Migrate();
                 Seed.SeedUsers(userManager, roleManager);
             }
 
